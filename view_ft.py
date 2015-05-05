fcab5fd2af180f54dd94e7aab97db539e18658d0
Dhruv Joshi LVP <dhruv.joshi.1989@gmail.com>
2015-05-05 10:37:30 +0530

    added file to view 2dft

---
+++ view_ft.py
@@ -0,0 +1,16 @@
+# view the ft of an image
+import numpy, pylab, matplotlib.cm, Image
+
+fileName = "./images/40.jpg"
+
+image = Image.open(fileName).convert("L")
+
+im = numpy.array(image, dtype=numpy.uint8)
+f = numpy.fft.fftshift(numpy.fft.fft2(im))
+p = numpy.abs(f)
+
+psd = Image.fromarray(p)
+
+pylab.figure()
+pylab.imshow(psd)
+pylab.show()
