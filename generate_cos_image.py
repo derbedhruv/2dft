5e63a6619e9aeb511a92bc5c90aad9e8db02aa05
Dhruv Joshi LVP <dhruv.joshi.1989@gmail.com>
2015-05-05 11:57:56 +0530

    scipy.misc.toimage works better than Image.fromarray

--- generate_cos_image.py
+++ generate_cos_image.py
@@ -1,8 +1,9 @@
 # Generate cosine image with given periods in x and y direction
 import numpy, pylab, scipy, Image, matplotlib.cm as cm
+from scipy.misc import toimage
 
-periodsx = 1.
-periodsy = 1.
+periodsx = 4.
+periodsy = 0.
 
 image_dimensions = [128., 128.]
 
@@ -14,11 +15,16 @@ for i in range(0, int(image_dimensions[0])):
 
 f = numpy.fft.fft2(image)
 psd = numpy.abs(numpy.fft.fftshift(f))
+# psd = Image.fromarray(psd)
+psd = toimage(psd)
 
-im = Image.fromarray(image)
+# im = Image.fromarray(image)
+im = toimage(image)
+psd.save('./images/ft_python.jpg')
 
 # fileName = raw_input("Enter filename to save as: ")
 # im.save(fileName)
+im.save('./images/40_python.jpg')
 
 pylab.figure()
 pylab.imshow(im,  cmap = cm.Greys_r)	# display the image
--- view_ft.py
+++ view_ft.py
@@ -1,7 +1,7 @@
 # view the ft of an image
 import numpy, pylab, matplotlib.cm, Image
 
-fileName = "./images/40.jpg"
+fileName = "./images/40_python.jpg"
 
 image = Image.open(fileName).convert("L")
 
